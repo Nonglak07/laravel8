a0c4e1f4386a3e84fa3639c53620788070c7b40c
Nonglak07 <130300978+Nonglak07@users.noreply.github.com>
2023-07-08 15:47:58 +0700

    Initial commit

---
+++ resources/views/test/cat.blade.php
@@ -0,0 +1,8 @@
+<html>
+
+<body>
+<h1>Gallery of Nonglak 64222420003</h1>
+    cat <img src="{{ $cat }}" style="height:180px;"> 
+</body>
+
+</html>
---
+++ resources/views/test/index.blade.php
@@ -0,0 +1,10 @@
+<html>
+
+<body>
+    <h1>Gallery of Nonglak 64222420003</h1>
+    <a href="http://127.0.0.1:8000/gallery/ant"> ant <img src="{{ $ant }}" style="height:150px;">
+        <a href="http://127.0.0.1:8000/gallery/bird"> bird <img src="{{ $bird }}" style="height:150px;">
+            <a href="http://127.0.0.1:8000/gallery/cat"> cat <img src="{{ $cat }}" style="height:150px;">
+</body>
+
+</html>
