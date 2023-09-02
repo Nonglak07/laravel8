f18c866a50ad8d3474406dba0e7a3c6ec207684c
Nonglak07 <130300978+Nonglak07@users.noreply.github.com>
2023-09-02 12:49:19 +0700

    week09

--- config/app.php
+++ config/app.php
@@ -176,6 +176,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        //week08
+        Collective\Html\HtmlServiceProvider::class,
+
     ],
 
     /*
@@ -230,6 +233,11 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+
+        'Form' => Collective\Html\FormFacade::class,
+        'HTML' => Collective\Html\HtmlFacade::class,
+
 
     ],
 
